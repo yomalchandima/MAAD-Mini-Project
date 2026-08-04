e9809ae72602722ab89b1f32145b1b818648ddca
Heshani Abeywarna <heshaniabeywarna@gmail.com>
2026-08-05 02:00:45 +0530

    fix any error in splash screen

--- app/src/main/java/com/example/maadminiproject/SplashActivity.kt
+++ app/src/main/java/com/example/maadminiproject/SplashActivity.kt
@@ -6,19 +6,22 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+import android.util.Log
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // Match the rest of the app's edge-to-edge style
-        enableEdgeToEdge()
+        Log.d("SplashActivity", "onCreate called")
 
         setContentView(R.layout.activity_splash)
 
         // Delay for 3 seconds and then start LoginActivity
         Handler(Looper.getMainLooper()).postDelayed({
+            Log.d("SplashActivity", "Moving to LoginActivity")
             val intent = Intent(this, com.example.maadminiproject.ui.authentication.LoginActivity::class.java)
             startActivity(intent)
             finish()
